050bd51cef04c3c542fb5e637f44afa63e9280af
Jonathan Chandra <chandrajonathan5@gmail.com>
2023-06-02 14:39:22 +0200

    Update on tfest

--- Measurement/Data_Extraction.m
+++ Measurement/Data_Extraction.m
@@ -21,7 +21,7 @@ for i = 28:39
     if p < 15000
         zero_crossings = find(diff(sign(out_lvdt{i-27})) ~= 0);
         start_index= zero_crossings(1);
-        end_index= zero_crossings(4);
+        end_index= zero_crossings(5);
     elseif p > 25000
         zero_crossings = find(diff(sign(out_lvdt{i-27})) ~= 0);
         start_index= zero_crossings(3);
@@ -39,3 +39,15 @@ for i = 28:39
     out_lvdt_trimmed{i-27} = out_lvdt{i-27}(start_index:end_index);
     time_trimmed{i-27} = time{i-27}(start_index:end_index);
 end
+
+Ts = 0.001;
+allData = [];
+for i = 1:numel(time_trimmed)
+    data = iddata(out_lvdt_trimmed{i},  inp_lvdt_trimmed{i}, Ts); 
+    allData = [allData; data]; 
+end
+
+opt = tfestOptions('Ts', Ts);
+sys = tfest(allData, 2, 2, opt); 
+
+% start validation here
